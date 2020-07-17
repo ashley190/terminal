616585422245c2a15f9031a2caa7da72d1bcd10b
Ashley Lam <ashley850819@gmail.com>
2020-07-17 05:45:47 +0000

    added --about and --help tags to print custom information

--- src/functions.py
+++ src/functions.py
@@ -9,6 +9,22 @@ text_3 = colored("exit", 'blue', attrs=['bold', 'underline'])
 text_4 = colored("y", 'blue', attrs=['bold', 'underline'])
 text_5 = colored("n", 'blue', attrs=['bold', 'underline'])
 
+about_txt = f"""Application: The Personal Wine Assistant
+    Creator: Ashley Lam
+    Purpose: To help users select appropriate wines for appropriate occasions
+    Conditions: No access for persons below legal drinking age
+    Credits: Wine Folly wine selection chart(https://media.winefolly.com/how-to-choose-wine-infographic.png#fullsize)
+        """
+
+help_txt = f"""
+The application will prompt for input at each step. Type in your input and press {text_1} at each point.
+
+exit:   You can exit the application by typing in {text_3}. Exit can be used at all points in the application EXCEPT for y/n steps.
+b:      During the wine selection process, entering {text_2} will enable stepping back through the selection process by one(1) step at a time. 
+            {text_2} can continually be used all the way up to the first selection question.
+        """
+
+
 welcome = f"""
 {colored("Welcome to your personal wine assistant.", attrs=['dark','underline'])} 
 I will help guide you through the often overwhelming process of selecting wine for different situations.
@@ -17,11 +33,11 @@ Here are some basic controls for this application:-
 1.  When prompted for your input, type in the required information/choices followed by {text_1} to submit your input. This applies to all inputs.
     If you've entered an invalid input, you will be prompted again to submit a new input.
 2.  Special inputs:-
-        {text_2} : During the selection process, submit 'b' to go back one step in the selection process.
-        {text_3}: Helps you exit the application. You can submit an 'exit' command at any point in the application other than a y/n step. 
-                You'll be prompted to confirm your exit by entering y/n. 
-                If {text_4} is chosen, the application will exit. 
-                If {text_5} is chosen, the application will continue to prompt you for information to proceed at the current step.
+        {text_2} :  During the selection process, submit {text_2} to go back one step in the selection process.
+        {text_3}:   Helps you exit the application. You can submit an {text_3} command at any point in the application other than a y/n step. 
+                    You'll be prompted to confirm your exit by entering {text_4}/{text_5}. 
+                    If {text_4} is chosen, the application will exit. 
+                    If {text_5} is chosen, the application will continue to prompt you for information to proceed at the current step.
                 
 Let's get started!
 """
--- src/main.py
+++ src/main.py
@@ -4,19 +4,25 @@ from functions import *
 from winelibrary import *
 import sys
 
+if "--about" in sys.argv and "--help" in sys.argv:
+    print(about_txt)
+    print(help_txt)
+elif "--about" in sys.argv:
+    print(about_txt)
+elif "--help" in sys.argv:
+    print(help_txt)
+else:
+    print(welcome)
+    check_age()
+    name = name()
+    print(f"Thank you, {name}. Let's select some wine!")
 
-print(welcome)
-check_age()
-name = name()
-print(f"Thank you, {name}. Let's select some wine!")
+    selection = wine_select(name)
+    print(selection)
 
-selection = wine_select(name)
-print(selection)
-
-if len(selection) > 1:
-    print("Here are all the wines we picked today...")
-    for item in selection:
-        print(selection[item][0])
-        print(selection[item][1])
-
-print("Enjoy!")
+    if len(selection) > 1:
+        print("Here are all the wines we picked today...")
+        for item in selection:
+            print(selection[item][0])
+            print(selection[item][1])
+    print("Enjoy!")
